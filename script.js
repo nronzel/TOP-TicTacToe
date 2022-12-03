895bd73c6b93d665b8b76b531501b735338aee14
nronzel <86695181+nronzel@users.noreply.github.com>
2022-12-03 13:44:30 -0500

    implemented turn counter to determine whose turn it is

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
-const domElementCache = {
-  "gridItem": Array.from(document.querySelectorAll("#gridItem")),
-  "restartBtn": document.querySelector("button.restart-btn"),
+const domCache = {
+  "gridItem": Array.from(document.querySelectorAll( "#gridItem" )),
+  "restartBtn": document.querySelector( "button.restart-btn" ),
+  // "marker": document.getElementById( "select" ),
 };
 
-// IIFE to listen for clicks to place an X
-(() => {
-  domElementCache.gridItem.forEach((item) => {
-    item.addEventListener("click", () => {
-      item.textContent = "X";
-    });
-  });
-})();
+// turn counter
+const counter = {
+  "count": 1, // if count == 10, no more turns left
+};
 
-// clears board
-const  restart = () => {
-  domElementCache.gridItem.forEach((item) => {
-    item.textContent = "";
+// event listener loop should be main game loop
+// inside the event listener loop it should check for two things:
+//  1. if there is already a letter in the selected square
+//  2. if the count % 2 == 0, playerTwo's turn, else playerOne's turn
+
+const gameLoop = () => {
+  domCache.gridItem.forEach(( item ) => {
+    item.addEventListener( "click", () => {
+      if ( counter.count % 2 == 0 ) {
+        item.textContent = playerTwo.marker;
+        counter.count++
+      } else {
+        item.textContent = playerOne.marker;
+        counter.count++
+      }
+    })
   })
 }
 
-domElementCache.restartBtn.addEventListener('click', restart)
+const player = ( marker ) => {
+  return { 
+    marker,
+    makeMove() {
+
+    }
+  }
+}
+
+const playerOne = player("X");
+const playerTwo = player("O");
+
+gameLoop();
+
+// clears board and resets turn counter
+const restart = () => {
+  domCache.gridItem.forEach(( item ) => {
+    item.textContent = "";
+    counter.count = 0;
+  })
+}
+
+domCache.restartBtn.addEventListener( 'click', restart )
